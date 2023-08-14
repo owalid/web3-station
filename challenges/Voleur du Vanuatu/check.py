33a59fff1a9ea6c4c1005daeb7308b46d88ac7a3
avan-pra <avan-pra@users.noreply.github.com>
2023-08-14 23:23:07 +0200

    add script to create the stealer challenge and add its answer

--- challenges/Voleur du Vanuatu/check.py
+++ challenges/Voleur du Vanuatu/check.py
@@ -10,6 +10,6 @@ def check(w3: Web3, abi: Dict, address: str, conn: socket, logger: logging.Logge
     if not answer:
         return False
     answer = answer.decode().strip()
-    if answer == "0x25":
+    if answer == "0xae42A3dDc82667012B4608942d6ab1534f1a543a0x0f69D850E415869a2dB986654381c525008c1E530x57d3ea422d9A4907Ad6373E03DeAc653633CEa980x40126b2E329e5fC7cAbb8CDB5c9F9FEB8Fe58A16":
         return True
     return False
