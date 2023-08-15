077cfe12580b0fec359d6aef02c1a4cfd6a13a4e
avan-pra <avan-pra@users.noreply.github.com>
2023-08-15 18:52:19 +0200

    add another step to complicate the challenge

--- challenges/To the Monaco/challenge.sol
+++ challenges/To the Monaco/challenge.sol
@@ -27,7 +27,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract PedestrainDonutSellerStock is ERC721 {
 
-    enum Buyables { DONUT, CHOUCHOU, CHICHI }
+    enum Buyables { DONUT, CHOUCHOU, CHICHI, ICE_CREAM }
     address public owner; 
 
     constructor() ERC721("SellerInventory", "SIE") {
@@ -35,23 +35,25 @@ contract PedestrainDonutSellerStock is ERC721 {
         _mint(msg.sender, uint256(Buyables.DONUT));
         _mint(msg.sender, uint256(Buyables.CHOUCHOU));
         _mint(msg.sender, uint256(Buyables.CHICHI));
+        _mint(msg.sender, uint256(Buyables.ICE_CREAM));
     }
 
     function buy() public payable {
         uint256 amount = msg.value;
 
-        require(amount >= 500 wei, "Hola amigos, tu va me le donner ton argent ou quoi ?");
+        require(amount >= 500 wei, "Hola amigos, tu vas me le donner ton argent ?");
         amount -= 500 wei;
         _transfer(owner, msg.sender, uint256(Buyables.DONUT));
 
-        require(amount >= 0.75 gwei, "Pas si pauvre... Mais mes chouchous te restent hors de portee");
+        require(amount >= 0.75 gwei, "Pas si pauvre... Mes chouchous te restent hors de portee !");
         amount -= 0.75 gwei;
         _transfer(owner, msg.sender, uint256(Buyables.CHOUCHOU));
 
-        require(amount >= 1.5 ether, "1.5 ether, vous savez combien ca fait 1.5 milliards de milliards Larmina");
+        require(amount >= 1.5 ether, "1.5 ether, vous savez combien ca fait 1.5 milliards de milliards Larmina ?");
         amount -= 1.5 ether;
         _transfer(owner, msg.sender, uint256(Buyables.CHICHI));
 
-        require(amount == 0, "tu n'es pas capable de faire l'appoint ???");
+        require(amount == 0, "Tu n'es pas capable de faire l'appoint ??? J'allais pourtant t'offrir de la glace");
+        _approve(msg.sender, uint256(Buyables.ICE_CREAM));
     }
 }
