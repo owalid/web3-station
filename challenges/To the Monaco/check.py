4ac183275e2f8d50e4d408484262be1da3c7865d
avan-pra <avan-pra@users.noreply.github.com>
2023-08-23 10:11:56 +0200

    Fix #4

--- challenges/To the Monaco/check.py
+++ challenges/To the Monaco/check.py
@@ -4,4 +4,4 @@ import logging
 from os import getenv
 
 def check(w3: Web3, abi: Dict, address: str, conn, logger: logging.Logger):
-    return all([w3.eth.contract(address=address, abi=abi).functions.ownerOf(x).call() != getenv("WEB3_PUBLIC_KEY") for x in range(0, 4)])
+    return all([w3.eth.contract(address=address, abi=abi).functions.ownerOf(x).call() != address for x in range(0, 4)])
